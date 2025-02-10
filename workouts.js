6cb20c30090fc9968d8bab8741f9799b5ad5328b
Arthur Waters <brighterdevv@gmail.com>
2025-02-10 23:04:31 +0000

    Updated the website

--- workouts.js
+++ workouts.js
@@ -8,13 +8,4 @@ const workouts = [
       exercises: ["Bicep Curls", "Hammer Curls", "Shoulder Press", "Lateral Raises", "Chest Press", "Tricep Curls", "Chest Flies", "Forearm Twists", "Cross-Body Curls", "Forearm Curls"],
       repetitions: 3
     },
-    {
-        name: "Dumbells Upper Body 2",
-        difficulty: "medium",
-        totalTime: "30:00",
-        workoutTime: "00:30",
-        breakTime: "00:30",
-        exercises: ["Bicep Curls", "Hammer Curls", "Shoulder Press", "Lateral Raises", "Chest Press", "Tricep Curls", "Chest Flies", "Forearm Twists", "Cross-Body Curls", "Forearm Curls"],
-        repetitions: 3
-      },
   ];
